b126a219b7d12b94c9cc7d78c7f3e23e90e528ec
Sierra Braun <braundsig@gmail.com>
2022-03-03 11:30:27 -0500

    update employee class

--- classes/employee.class.php
+++ classes/employee.class.php
@@ -8,6 +8,37 @@
 
 class Employee extends Person{
 
+    //private data member for ssn
+    private $ssn;
+
+    private static $employee_count = 0;
+
     //constructor
-    public function ($first_name, $last_name, $ssn, $employee_count)
+    public function __construct($first_name, $last_name, $ssn, $employee_count){
+        parent::__construct($first_name, $last_name);
+        $this->ssn = $ssn;
+        self::$employee_count++;
+    }
+
+    //get methods
+    public function getSsn(){
+        return $this->ssn;
+    }
+
+    public static function getEmployeeCount(){
+        return self::$student_count;
+    }
+
+
+    //set methods
+    public function setSsn(){
+        $this->ssn = $ssn;
+    }
+
+
+    //tostring
+    public function toString(){
+        parent::toString();
+        echo "SSN: ", $this->getSsn();
+    }
 }
