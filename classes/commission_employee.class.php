7dfc5f2c831564d603a0f31b7401b760ece40726
kavthakk <77465190+kavthakk@users.noreply.github.com>
2022-03-03 11:14:04 -0500

    filling in foundations of files

--- classes/commission_employee.class.php
+++ classes/commission_employee.class.php
@@ -1,7 +1,33 @@
 <?php
+
 /**
  * Author: Kavin Thakkar
  * Date: 3/1/22
  * File: commission_employee.class.php
- * Description:
+ * Description: The commission_employee.class.php file contains the CommissionEmployee class, which
+ * also inherits from the Employee class.
  */
+class CommissionEmployee extends Employee
+{
+    private $sales, $commission_rate;
+
+    public function __construct($name, $title, $years, $salary) {
+        parent::__construct($name, $title, $years, $salary);
+    }
+
+    public function getSales() {
+        return $this->sales;
+    }
+
+    public function getCommissionRate() {
+        return $this->commission_rate;
+    }
+
+    public function getPaymentAmount() {
+        return $this->payment_amount;
+    }
+
+    public function toString() {
+//echo it up!
+    }
+}
